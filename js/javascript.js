ba93c2d2e51c4344d1875932d97d668c0df645c4
Yessi Kenney <yessikenney@gmail.com>
2024-12-29 08:00:22 -0800

    publishing rock paper scissors console game

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+let add7 = (number) => number + 7;
+/*
+let message; 
+message = "hello!";
+message = "world!";
+// to be concise can type let message = "hello!" and then the alert like below
+alert(message); */
+/*
+let hello = 'Hello world!!!';
+let message; 
+
+message = hello;
+
+alert(message);
+*/
+
+
